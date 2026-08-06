dfe6edda11a67f7899189882d78f2d33b7a23fc5
Igor Józefowicz <igorjozefowicz@gmail.com>
2026-08-07 00:34:28 +0200

    Refactor code structure for improved readability and maintainability

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "@fontsource/barlow-condensed/700.css";
+import "@fontsource/barlow-condensed/800.css";
+import "@fontsource/barlow-condensed/900.css";
+import "@fontsource/manrope/400.css";
+import "@fontsource/manrope/600.css";
+import "@fontsource/manrope/700.css";
+import "@fontsource/manrope/800.css";
 import { App } from "./App";
 import "./styles.css";
 
